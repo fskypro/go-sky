48476f0287c8102b2ee2868b2dfe235980e4ab56
fanky <fanky@fsky.pro>
2020-06-10 23:36:02 +0800

    add function fsnet.InetAtoN() and fsnet.InetNtoA

--- fsnet/net.go
+++ fsnet/net.go
@@ -12,6 +12,7 @@ import "fmt"
 import "net"
 import "strconv"
 import "strings"
+import "math/big"
 
 // -------------------------------------------------------------------
 // 主机结构
@@ -72,67 +73,72 @@ func GetFreePort() (port int, err error) {
 // -------------------------------------------------------------------
 // IP 地址转换函数
 // -------------------------------------------------------------------
+// 将四字节整数转换为字符串形式 IP 地址
+// 类似 C 语言的 inet_ntoa
+func InetNtoA(ip int64) string {
+	return fmt.Sprintf("%d.%d.%d.%d", byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip))
+}
+
+// 将 IP 地址转换为四字节整数
+// 类似 C 语言的 inet_aton
+// 注意：如果是不合法的 ip 地址，返回 -1
+func InetAtoN(ip string) int64 {
+	netIP := net.ParseIP(ip)
+	if netIP == nil {
+		return -1
+	}
+	ret := big.NewInt(0)
+	ret.SetBytes(netIP.To4())
+	return ret.Int64()
+}
+
+// --------------------------------------------------------
 // 将 IP 地址和端转换为 UINT64 值
 // 参数 addr 格式可以为 aa.bb.cc.dd:port 或者 aa.bb.cc.dd
 // 如果是前者，则参数 port 不起作用
 // 返回：(aa << 24) + (bb << 16) + (cc << 8) + dd + (port << 32)
-//	注意：如果解释失败，则返回 -1
+//	注意：
+//		如果地址部分为空字符串，则地址将被置为 0.0.0.0
+//		如果解释失败，则返回 -1
 func AddrToInt64(addr string, port uint16) int64 {
+	var a string
+	p := int64(port)
 	ap := strings.Split(addr, ":")
 	if len(ap) == 0 {
-		return -1
-	}
-	segs := strings.Split(ap[0], ".")
-	if len(segs) != 4 {
-		return -1
-	}
-	aa, err := strconv.ParseUint(segs[0], 10, 8)
-	if err != nil {
-		return -1
-	}
-	bb, err := strconv.ParseUint(segs[1], 10, 8)
-	if err != nil {
-		return -1
-	}
-	cc, err := strconv.ParseUint(segs[2], 10, 8)
-	if err != nil {
-		return -1
-	}
-	dd, err := strconv.ParseUint(segs[3], 10, 8)
-	if err != nil {
-		return -1
-	}
-	vaddr := (aa << 24) + (bb << 16) + (cc << 8) + dd
-
-	// 地址中带有端口
-	if len(ap) > 1 {
-		vport, err := strconv.ParseUint(ap[1], 10, 16)
+		a = "0.0.0.0"
+	} else if len(ap) == 1 {
+		a = ap[0]
+	} else if len(ap) == 2 {
+		a = ap[0]
+		up, err := strconv.ParseUint(ap[1], 10, 16)
 		if err != nil {
 			return -1
 		}
-		return int64(vaddr + (vport << 32))
+		p = int64(up)
 	}
-
-	// 端口号放在 port 参数中
-	if port > 0 {
-		return int64(vaddr + uint64(port)<<32)
+	ip := InetAtoN(a)
+	if ip == -1 {
+		return -1
 	}
-
-	return int64(vaddr)
+	return ip + (p << 32)
 }
 
 // 将数值形式的 IP 地址，转换为字符串形式，格式为 aa.bb.cc.dd:port
 // 如果数值小于
-func Int64ToAddr(vaddr int64) string {
+func Int64ToAddr(iaddr int64) string {
 	const minPort int64 = int64(1 << 32)
 
-	aa := (vaddr >> 24) & int64(255)
-	bb := (vaddr >> 16) & int64(255)
-	cc := (vaddr >> 8) & int64(255)
-	dd := vaddr & int64(255)
-	if vaddr < minPort { // 没有端口
-		return fmt.Sprintf("%d.%d.%d.%d", aa, bb, cc, dd)
+	if iaddr < minPort { // 没有端口
+		return fmt.Sprintf("%d.%d.%d.%d",
+			byte(iaddr>>24),
+			byte(iaddr>>16),
+			byte(iaddr>>8),
+			uint16(iaddr))
 	}
-	port := vaddr >> 32
-	return fmt.Sprintf("%d.%d.%d.%d:%d", aa, bb, cc, dd, port)
+	return fmt.Sprintf("%d.%d.%d.%d:%d",
+		byte(iaddr>>24),
+		byte(iaddr>>16),
+		byte(iaddr>>8),
+		byte(iaddr),
+		uint16(iaddr>>32))
 }
--- fsnet/net_test.go
+++ fsnet/net_test.go
@@ -20,8 +20,15 @@ func TestGetFreePort(t *testing.T) {
 func TestInt64ToAddr(t *testing.T) {
 	fstest.PrintTestBegin("Int64ToAddr/AddrToInt64")
 
+	fmt.Printf(`call InetAtoN("192.168.1.2"): `)
+	vaddr := InetAtoN("192.168.1.2")
+	fmt.Println(vaddr)
+	fmt.Printf("call InetNtoA(%d):  ", vaddr)
+	fmt.Println(InetNtoA(vaddr))
+	fmt.Println("")
+
 	fmt.Printf(`call AddrToInt64("192.168.2.56", 0): `)
-	vaddr := AddrToInt64("192.168.2.56", 0)
+	vaddr = AddrToInt64("192.168.2.56", 0)
 	fmt.Println(vaddr)
 	fmt.Printf("call Int64ToAddr(%d):  ", vaddr)
 	fmt.Println(Int64ToAddr(vaddr))
